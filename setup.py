7d45be0d05b8d8711d5df147517f1ed2c6748343
Nemesis <kian.khaje@gmail.com>
2025-05-25 21:21:03 +0200

    Bumped revision

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pyconfix",
-    version="0.4.5",
+    version="0.4.6",
     description="A simple feature managment tool library",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
